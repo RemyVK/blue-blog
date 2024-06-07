70795d3db2661561e3a388f08b03e83e36a2c64f
RemyVK <remykaredan@gmail.com>
2024-06-07 16:47:28 +0200

    Added Basic styles

--- components/BlogPageLayout.tsx
+++ components/BlogPageLayout.tsx
@@ -1,14 +1,9 @@
 import DataTable from "./DataTable";
 
-type BlogPageLayoutProps = {
-  children?: React.ReactNode; // Made children optional
-};
-
-export default function BlogPageLayout({ children }: BlogPageLayoutProps) {
+export default function BlogPageLayout() {
   return (
     <>
       <DataTable />
-      {children}
     </>
   );
 }
