91cc63a80406e61feb2f6bbf0f2d624934ce7aad
RemyVK <remykaredan@gmail.com>
2024-06-07 02:03:33 +0200

    Create Context for Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import PostsLayout from "../components/Postslayout";
+import type { AppProps } from "next/app";
+import PostContextProvider from "../context/PostsContext";
 
-export default function Home() {
-  return <PostsLayout />;
+export default function Home({ Component, pageProps }: AppProps) {
+  return (
+    <PostContextProvider>
+      <Component {...pageProps}></Component>
+    </PostContextProvider>
+  );
 }
